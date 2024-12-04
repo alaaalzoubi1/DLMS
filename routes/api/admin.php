4f7c516cfbd5db72d68b7630063e2fa352afdbf3
alaaalzoubi1 <alaaalzoubu321@gmail.com>
2024-12-05 00:01:16 +0300

    clinics_with_the_special_price

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -49,7 +49,7 @@ Route::middleware(['auth:admin', 'admin.role'])->group(function () {
     Route::post('add-special-price', [ClinicController::class, 'addSpecialPrice']);
     Route::delete('delete-special-price', [ClinicController::class, 'deleteSpecialPrice']);
     Route::patch('availability/{id}', [UserController::class, 'setAvailability']);
-    Route::get('clinics_with_special_price/{id}',[ClinicController::class,'clinics_with_special_price']);
+    Route::get('clinics_with_special_price/{$subscriberId}',[ClinicController::class,'clinics_with_special_price']);
     Route::get('get_clinics_with_the_special_price/{id}',[ProductController::class,'get_clinics_with_the_special_price']);
 
 });
